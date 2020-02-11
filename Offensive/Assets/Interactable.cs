2dfeae9d57ae19cfbabbe22c47532e06b412c1c9
HorthyMiklos <60073537+HorthyMiklos@users.noreply.github.com>
2020-02-11 19:02:07 +0100

    2020. 02. 11.

---
+++ Offensive/Assets/Interactable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    private Vector3 scale = new Vector3(0.0001f, 0.0001f, 0.0001f);
+    private Vector3 originalScale;
+    public int cameraControll;
+    [SerializeField] private bool canChangeScale = false;
+
+    private void Start()
+    {        
+        originalScale = gameObject.transform.localScale;
+    }
+
+    void OnMouseEnter()
+    {
+        if (canChangeScale)
+        {
+            gameObject.transform.localScale = originalScale + scale;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        if (canChangeScale)
+        {
+            gameObject.transform.localScale = originalScale;
+        }
+    }
+
+}
--- Offensive/Assets/MenuManager.cs
+++ Offensive/Assets/MenuManager.cs
@@ -8,30 +8,141 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private GameObject menuCamera;
     [SerializeField] private Vector3 camPos1;
     [SerializeField] private Vector3 camRot1;
-    private Quaternion rot1;
     [SerializeField] private Vector3 camPos2;
     [SerializeField] private Vector3 camRot2;
-    private Quaternion rot2;
+    [SerializeField] private Vector3 camPos3;
+    [SerializeField] private Vector3 camRot3;
+    [SerializeField] private Vector3 camPos4;
+    [SerializeField] private Vector3 camRot4;
+    
+    
+    [SerializeField] private GameObject Canvas;
+    [SerializeField] private GameObject Background;
+    [SerializeField] private GameObject KurszkUI;
+    [SerializeField] private GameObject BerlinUI;
 
-    public AnimationCurve curve;
+    private Quaternion camRotation;
+
+    private Vector3 difference;
+    private Vector3 finalPos;
+    private Quaternion rotDifference;
+    private Quaternion finalRot;
+
+    private float lerpEndValue;
+    [SerializeField] private float lerpSpeed;
+
+    [SerializeField] private LayerMask raycastLayer;
+
+    [SerializeField] private GameObject tableLight;
 
     // Start is called before the first frame update
     void Start()
     {
-        rot1.eulerAngles = camRot1;
-        rot2.eulerAngles = camRot2;
+        finalPos = camPos1;
+        finalRot.eulerAngles = camRot1;
+        KurszkUI.SetActive(false);
+        BerlinUI.SetActive(false);
+        Background.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        #region lerp
+
+        lerpEndValue = lerpSpeed / 1000f;
+
+        #region position
+        menuCamera.transform.position = Vector3.Lerp(menuCamera.transform.position, finalPos, lerpSpeed * Time.deltaTime);
+
+        difference = menuCamera.transform.position - finalPos;
+
+        if (Mathf.Abs(difference.x) < lerpEndValue && Mathf.Abs(difference.y) < lerpEndValue && Mathf.Abs(difference.z) < lerpEndValue)
         {
-            menuCamera.transform.position = Vector3.Slerp(menuCamera.transform.position, camPos2, curve.Evaluate(Time.deltaTime));
+            menuCamera.transform.position = finalPos;
+        }
+
+        #endregion
+
+        #region rotation
+
+        menuCamera.transform.rotation = camRotation;
+        camRotation = Quaternion.Lerp(menuCamera.transform.rotation, finalRot, lerpSpeed * Time.deltaTime);
+
+        rotDifference.eulerAngles = menuCamera.transform.rotation.eulerAngles - finalRot.eulerAngles;
+
+        if (Mathf.Abs(rotDifference.eulerAngles.x) < lerpEndValue && Mathf.Abs(rotDifference.eulerAngles.y) < lerpEndValue && Mathf.Abs(rotDifference.eulerAngles.z) < lerpEndValue)
+        {
+            menuCamera.transform.rotation = finalRot;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region menuManager
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(menuCamera.transform.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, raycastLayer))
+            {
+                if (hit.transform.GetComponent<Interactable>().cameraControll == 2)
+                {
+                    finalPos = camPos2;
+                    finalRot.eulerAngles = camRot2;
+                }
+                if (hit.transform.GetComponent<Interactable>().cameraControll == 3)
+                {
+                    finalPos = camPos3;
+                    finalRot.eulerAngles = camRot3;
+                    KurszkUI.SetActive(true);
+                    Background.SetActive(true);
+                }
+                else
+                {
+                    KurszkUI.SetActive(false);
+                }
+
+                if (hit.transform.GetComponent<Interactable>().cameraControll == 4)
+                {
+                    finalPos = camPos4;
+                    finalRot.eulerAngles = camRot4;
+                    BerlinUI.SetActive(true);
+                    Background.SetActive(true);
+                }
+                else
+                {
+                    BerlinUI.SetActive(false);
+                }
+            }
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Background.SetActive(false);
+            if (finalPos == camPos2)
+            {
+                finalPos = camPos1;
+                finalRot.eulerAngles = camRot1;
+            }
+            if (finalPos != camPos1 && finalPos != camPos2)
+            {
+                finalPos = camPos2;
+                finalRot.eulerAngles = camRot2;
+            }
+        }
+
+        if (menuCamera.transform.position == camPos1)
+        {
+            tableLight.SetActive(true);
         }
         else
         {
-            menuCamera.transform.position = Vector3.Slerp(menuCamera.transform.position, camPos1, curve.Evaluate(Time.deltaTime));
+            tableLight.SetActive(false);
         }
     }
+
+        #endregion
 }
